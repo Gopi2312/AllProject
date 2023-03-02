04fc6abbe19f7f548a9372803e686378f547adfb
Gopi2312 <123471487+Gopi2312@users.noreply.github.com>
2023-03-02 19:52:29 +0530

    commit

--- src/main/java/assignment/allproject/tictactoa/Check.java
+++ src/main/java/assignment/allproject/tictactoa/Check.java
@@ -7,25 +7,28 @@ abstract class Check
 	Logger logger = Logger.getLogger("hi");
 	int size;
 	char[][] ch;
-    String str1 = "Player 1 Winner";
-    String str2 = "Player 2 Winner";
+    	String str1 = "Player 1 Winner";
+    	String str2 = "Player 2 Winner";
 	Check(int size,char[][] ch)
 	{
 		this.size = size;
 		this.ch = ch;
 	}
-	protected void winner(int flag,int flag1)
+	protected int winner(int flag,int flag1)
 	{
 		if(flag == size)
 		{
 			logger.info(str1);
-			Tictactoa.i = (size*size)+1;
+			return size*size+1;
 		}
 		if(flag1 == size)
 		{
 			logger.info(str2);
-			Tictactoa.i=(size*size)+1;
+			return size*size+1;
+		}
+		else {
+		return 0;
 		}
 	}
-    protected abstract void check();
+    protected abstract int check();
 }
