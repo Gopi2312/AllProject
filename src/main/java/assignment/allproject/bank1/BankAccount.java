4b6b099d745bfdc8b07bf2ee147e8a3de4b18b8b
Gopi2312 <123471487+Gopi2312@users.noreply.github.com>
2023-03-02 18:30:16 +0530

    commit

--- src/main/java/assignment/allproject/bank1/BankAccount.java
+++ src/main/java/assignment/allproject/bank1/BankAccount.java
@@ -3,16 +3,15 @@ import java.util.logging.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class BankAccount{
-	final static Logger log = Logger.getLogger("hi");
 	protected BankAccount() {
-		log.info("Welcmo to BankAccount");
+		
 	}
 	public static void run()
-    {
+    	{
 		String name = "";
 	    long acno = 0;
 	    double balance = 0.0;
-	    
+	    Logger log = Logger.getLogger("hi");
 	    Scanner sc = new Scanner(System.in);
         try {
             log.info("Enter Your Name:");
